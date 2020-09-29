1d83e0f2b09d19fdb6ba2cc781e0b8d29f8aa62b
Jesss-code <63443193+Jesss-code@users.noreply.github.com>
2020-09-30 04:30:18 +0800

    a

--- app/src/main/java/jsh/dev/kotlinsimpleconverter/ConvertWeight.kt
+++ app/src/main/java/jsh/dev/kotlinsimpleconverter/ConvertWeight.kt
@@ -1,11 +1,21 @@
 package jsh.dev.kotlinsimpleconverter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+import android.text.InputType
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_convert_weight.*
+
 
 class ConvertWeight : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_convert_weight)
+
+        button1.setOnClickListener {
+            Toast.makeText(this, "Try", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/jsh/dev/kotlinsimpleconverter/MainActivity.kt
+++ app/src/main/java/jsh/dev/kotlinsimpleconverter/MainActivity.kt
@@ -1,11 +1,56 @@
 package jsh.dev.kotlinsimpleconverter
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.get
+import kotlinx.android.synthetic.main.activity_convert_weight.*
 
 class MainActivity : AppCompatActivity() {
+    private var inputActive = 1
+    var convertFrom: String = "Nanometers"
+    var convertTo: String = "Nanometers"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_convert_weight)
+        var input :String = input1.text as String
+
+        seeSelected.setOnClickListener {
+            Toast.makeText(this, "From  $convertFrom to $convertTo" , Toast.LENGTH_SHORT).show()
+        }
+
+        input1.setOnClickListener {
+            setInputActive(1)
+
+        }
+        input2.setOnClickListener {
+            setInputActive(2)
+        }
+
+
+
+    }
+
+    private fun setInputActive(i: Int) {
+        if (i == 1) {
+            input1.setTypeface(null, Typeface.BOLD)
+            input2.setTypeface(null, Typeface.NORMAL)
+            this.inputActive = 1
+            this.convertFrom = spinner1.selectedItem.toString()
+            this.convertTo = spinner2.selectedItem.toString()
+        } else{
+            input2.setTypeface(null, Typeface.BOLD)
+            input1.setTypeface(null, Typeface.NORMAL)
+            this.inputActive = 2
+            this.convertFrom = spinner2.selectedItem.toString()
+            this.convertTo = spinner1.selectedItem.toString()
+        }
     }
 }
