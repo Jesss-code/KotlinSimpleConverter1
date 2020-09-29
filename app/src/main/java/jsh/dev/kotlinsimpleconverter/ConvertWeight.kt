289d2ea2f3fd1490c1d1d648640c7e48bd5b0323
Jesss-code <63443193+Jesss-code@users.noreply.github.com>
2020-09-29 21:39:47 +0800

    a

---
+++ app/src/main/java/jsh/dev/kotlinsimpleconverter/ConvertWeight.kt
@@ -0,0 +1,11 @@
+package jsh.dev.kotlinsimpleconverter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConvertWeight : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_convert_weight)
+    }
+}
